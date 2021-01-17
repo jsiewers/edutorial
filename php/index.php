1025680e73f56a0a54d6cbabed9947be25a75ed9
janjaap <janjaap@roc-dev.com>
2021-01-17 19:51:06 +0100

    php -> nodejs

---
+++ php/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="nl">`
+<?php include("header.php"); ?>
+
+<body>
+    <?php include("nav.php"); ?>
+    <div class="content">
+        <main>
+            <article class="header">
+                <h1>Meer leren over design, IT en software development</h1>
+            </article> <!-- article-header -->
+            <p class="col-1">Design. Op deze site vind je lesmateriaal en opdrachten waar design een rol bij speelt. Je
+                leert hier niet zozeer vorm te geven, maar je leert hier wel om te gaan met userinterface guidelines en
+                techniek om designs om te zetten in werkende applicaties en producten.<br />
+                Er is hier vooral veel te doen als het om software development gaat. Veel materiaal is afgestemd op het
+                kwalificatiedossier van de mbo-4 opleiding software developer. Het gaat om beginnerscursussen met een
+                gestructureerde opbouw tot aan artikelen over de laatste ontwikkelingen.
+
+            </p>
+            <p class="col-2">
+                IT. De rol van software developers bij het beheer van applicaties wordt steeds belangrijker. Doordat
+                applicaties door teams worden ontwikkeld zijn ze onderhevig aan veel aanpassingen en veranderingen. Om
+                er toch voor te zorgen dat al die veranderingen goed zijn getest en goed werken moet een development
+                team werken met software om versies in ontwikkeling en in productie te beheren. Denk hierbij aan
+                versiesystemen en tools om het uploaden en configureren van software te automatiseren en te testen.
+            </p>
+        </main>
+
+        <div id="cards">
+            <div class="card">
+                <h4>Verdieping software</h4>
+                <p>
+                    Verdieping software is een keuzedeel of MBO-certificaat waarbij je je gaat verdiepen in bepaalde
+                    softare. Onder software kun je een programma verstaan, maar ook frameworks, cms-en of andere
+                    tools
+                    die je gebruikt bij software development. Hier vind je tutorials over: Wordpress, drupal, slim,
+                    laravel voor php. Bootstrap en Bootstrap Studio
+                </p>
+            </div>
+            <div class="card">
+                <h4>Front end development</h4>
+                <p>
+                    Dit keuzedeel of MBO-certificaat is een verdieping op basis webdevelopment. Als je de basis van
+                    HTML, CSS en Javascript onder de knie hebt kun je met Frontend Development (K0722) aan de slag.
+                    Hier
+                    vind je verschillende onderwerpen die aansluiten bij dit keuzedeel, te weten: Variable fonts,
+                    PWA,
+                    SEO
+                </p>
+            </div>
+            <div class="card">
+                <h4>DevOps voor software developers</h4>
+                <p>
+                    Dit keuzedeel/MBO-certificaat is nog in ontwikkeling (jan. 2021). Het wordt ontwikkeld in
+                    samenwerking met o.a. Cisco. Hier vind je materiaal over o.a. Ansible, Docker, Google
+                    Cloudplatform,
+                    Git.
+                </p>
+            </div>
+            <div class="card">
+                <h4>DevOps voor software developers</h4>
+                <p>
+                    Dit keuzedeel/MBO-certificaat is nog in ontwikkeling (jan. 2021). Het wordt ontwikkeld in
+                    samenwerking met o.a. Cisco. Hier vind je materiaal over o.a. Ansible, Docker, Google
+                    Cloudplatform,
+                    Git.
+                    <a href="learningpath.php">Lees meer</a>
+                </p>
+            </div>
+        </div> <!-- cards -->
+    </div> <!-- content -->
+    <?php include("footer.php"); ?>
+</body>
+
+</html>
